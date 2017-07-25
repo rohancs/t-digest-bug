3ee290b126133a3da87eb2ab94491d283b5ace0f
Rohan Sharma <rsharma@netflix.com>
2017-07-25 10:49:25 -0400

    tweak

--- src/main/java/TDigestBug.java
+++ src/main/java/TDigestBug.java
@@ -15,19 +15,24 @@ public class TDigestBug {
         */
 
         TDigest digest = new AVLTreeDigest(10);
-        digest.add(addInput(10,300000000));
-        digest.add(addInput(200,300000000));
-        digest.add(addInput(3000,300000000));
-        digest.add(addInput(4000,300000000));
-        digest.add(addInput(5000,300000000));
-        digest.add(addInput(47883554,200));
+        for(int i=0; i<5;i++) {
+            System.out.println("******Iteration "+(i+1)+": ******");
+            digest.add(getDigest());
 
-        System.out.println("Media = "+digest.quantile(0.5));
+            System.out.println("Count = "+digest.size()+"\t Median = "+digest.quantile(0.5));
+        }
     }
 
-    static TDigest addInput(double x, int y) {
+    static TDigest getDigest() {
         TDigest digest = new AVLTreeDigest(10);
-        digest.add(x,y);
+
+        digest.add(10, 300000000);
+        digest.add(200, 300000000);
+        digest.add(3000, 300000000);
+        digest.add(4000, 300000000);
+        digest.add(5000, 300000000);
+        digest.add(47883554, 200);
+
         return digest;
     }
 }
